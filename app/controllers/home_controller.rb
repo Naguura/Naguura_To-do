2f2f5bbd9669ea9edbae32c3fca21b399c5ca8d8
Nyuxi10 <yasamaru03@gmail.com>
2019-12-01 12:08:55 -0500

    detalles front para los botones inferiores

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,13 +3,13 @@ class HomeController < ApplicationController
 	def delete_all
 	    @delete_all = List.destroy_all
 	    respond_to do |format|
-	      format.html { redirect_to lists_url, notice: 'all tasks was successfully destroyed.' }
+	      format.html { redirect_to lists_url, notice: 'All tasks was successfully destroyed.' }
 	      format.json { head :no_content }
 	    end  
   	end
   	def delete_crossed
 		@delete_crossed = List.where(:status => true).destroy_all
-		redirect_to root_url, notice: 'All crossed tasks were successfully destroyed' 
+		redirect_to root_url, notice: 'All completed tasks were successfully destroyed' 
 	end
 
 end
